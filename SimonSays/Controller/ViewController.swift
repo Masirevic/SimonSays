cc6987e59afff5c213bf50bf16b6926d0cc14b12
Ljuba <ljmasirevic@gmail.com>
2018-03-09 12:04:59 +0100

    Playing Color Sequences and Repeating Color Sequences

--- SimonSays/Controller/ViewController.swift
+++ SimonSays/Controller/ViewController.swift
@@ -17,6 +17,13 @@ class ViewController: UIViewController {
     @IBOutlet var playerLabels: [UILabel]!
     @IBOutlet var scoreLabels: [UILabel]!
     
+    var currentPlayer = 0
+    var scores = [0, 0]
+    
+    var sequenceIndex = 0
+    var colorSequence = [Int]()
+    var colorsToTap = [Int]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -29,23 +36,98 @@ class ViewController: UIViewController {
 
     
     func createNewGame () {
+        colorSequence.removeAll()
+        
+        
         actionButton.setTitle("Start Game", for: .normal)
+        actionButton.isEnabled = true
         for button in colorButtons {
             button.alpha = 0.5
+            button.isEnabled = false
+        }
+    }
+    
+    func addNewColor () {
+        colorSequence.append(Int(arc4random_uniform(UInt32(4))))
+    }
+    
+    func playSequence () {
+        if sequenceIndex < colorSequence.count {
+            flash(button: colorButtons[colorSequence[sequenceIndex]])
+            sequenceIndex += 1
+        } else {
+            colorsToTap = colorSequence
+            view.isUserInteractionEnabled = true
+            actionButton.setTitle("Tap the Circles", for: .normal)
+            for button in colorButtons {
+                button.isEnabled = true
+            }
+        }
+    
+    }
+    
+    func flash (button: CircularButton) {
+        UIView.animate(withDuration: 0.5, animations: {
+            button.alpha = 1.0
+            button.alpha = 0.5
+        }) { (bool) in
+            self.playSequence()
         }
     }
     
     
     @IBAction func colorButtonHandler(_ sender: CircularButton) {
-        print("Button \(sender.tag) tapped")
+        if sender.tag == colorsToTap.removeFirst() {
+            
+        } else {
+            for button in colorButtons {
+                button.isEnabled = false
+            }
+            return
+        }
+        
+        if colorsToTap.isEmpty {
+            for button in colorButtons {
+                button.isEnabled = false
+                
+            }
+            actionButton.setTitle("Continue", for: .normal)
+            actionButton.isEnabled = true
+            
+        }
     }
     
     
    
     @IBAction func actionButtonHandler(_ sender: UIButton) {
-        print("Action Button")
+        sequenceIndex = 0
+        actionButton.setTitle("Memorize", for: .normal)
+        actionButton.isEnabled = false
+        view.isUserInteractionEnabled = false
+        addNewColor()
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1)) {
+            self.playSequence()
+        }
+        
     }
     
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
